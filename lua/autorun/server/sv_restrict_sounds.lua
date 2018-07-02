ce9551bc9589070f0185391c4f910af3c03d7c7a
Themichaelreimer <themichaelreimer@gmail.com>
2018-07-02 12:59:55 -0500

    restrictedSounds -> soundIsRestricted; tabs -> 4 spaces
    Resolves issue #1

--- lua/autorun/server/sv_restrict_sounds.lua
+++ lua/autorun/server/sv_restrict_sounds.lua
@@ -1,13 +1,13 @@
-local restrictedSounds = {}
+local soundIsRestricted = {}
 
 -- Toolgun Sounds
-restrictedSounds["weapons/airboat/airboat_gun_lastshot1.wav"] = true
-restrictedSounds["weapons/airboat/airboat_gun_lastshot2.wav"] = true
+soundIsRestricted["weapons/airboat/airboat_gun_lastshot1.wav"] = true
+soundIsRestricted["weapons/airboat/airboat_gun_lastshot2.wav"] = true
 
 -- Gross thruster sounds
-restrictedSounds["ambient/gas/cannister_loop.wav"] = true
+soundIsRestricted["ambient/gas/cannister_loop.wav"] = true
 
 hook.Add("EntityEmitSound", "CFC_RestrictSounds", function(data)
-	local soundName = data["SoundName"]
-	if restrictedSounds[soundName] then return false end
+    local soundName = data["SoundName"]
+    if soundIsRestricted[soundName] then return false end
 end)
