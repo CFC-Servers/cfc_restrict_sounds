572b55a189403c071e5326261d8e269d000c6ab5
Brandon Sturgeon <sturgeonb4@gmail.com>
2018-10-25 14:25:04 -0700

    Logical renaming

--- lua/autorun/client/cl_restrict_sounds.lua
+++ lua/autorun/client/cl_restrict_sounds.lua
@@ -3,7 +3,7 @@ soundIsRestricted["weapons/airboat/airboat_gun_lastshot1.wav"] = true
 soundIsRestricted["weapons/airboat/airboat_gun_lastshot2.wav"] = true
 soundIsRestricted["ambient/gas/cannister_loop.wav"]            = true
 
-local function restrictSound( data )
+local function shouldPlaySound( data )
     local path = data.SoundName
     if soundIsRestricted[path] then return false end
 end
@@ -11,4 +11,4 @@ end
 local hookName = "CFC_MuteRestrictedSounds"
 
 hook.Remove( "EntityEmitSound", hookName )
-hook.Add( "EntityEmitSound", hookName, restrictSound )
+hook.Add( "EntityEmitSound", hookName, shouldPlaySound )
